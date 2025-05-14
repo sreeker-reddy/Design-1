52b2ad742088031a16f9c86bae1aa9d9b1a62e4c
sreeker-reddy <116924654+sreeker-reddy@users.noreply.github.com>
2025-05-13 21:14:01 -0700

    Update Design_HashSet.cs

--- Design_HashSet.cs
+++ Design_HashSet.cs
@@ -1,141 +1,49 @@
-/* Time Complexity: O(log(n/2003) = O(log(n))
-   Space Complexity: O(2003+n) = O(n)
+/* Time Complexity: O(1)
+   Space Complexity: O(n)
 
-   Approach: Choosing a prime number as the number of BST trees enables a more uniform distribution. As binary search trees are more efficient at operations performed on them for searching,
-             adding and removing, it is a good choice for a HashSet.
+   Approach: Computing the initial hash of the  number by using modulo square of the range for the first dimension and divide by square root of the range for the second dimension gives us the exact boolean array element. If the element is added to the array then the corresponding array element computed by hash functions is set to true.
 */
 
-public class Node
-{
-    public int val;
-    public Node left;
-    public Node right;
-    public Node(int val)
-    {
-        this.val = val;
-        this.left = null;
-        this.right = null;
-    }
-}
-
-public class BST
-{
-    public Node node;
-
-    public Node Search(Node root, int val)
-    {
-        if(root==null || root.val==val)
-        {
-            return root;
-        }
-        return val<root.val ? Search(root.left,val) : Search(root.right,val);
-    }
-
-    public Node Add(Node root, int val)
-{
-    if (root == null)
-        return new Node(val);
-
-    if (val < root.val)
-        root.left = Add(root.left, val);
-    else if (val > root.val)
-        root.right = Add(root.right, val);
-    return root;
-}
-
-    public Node Delete(Node root, int val)
-    {
-        if(root==null)
-        {
-            return null;
-        }
-
-        if(val>root.val)
-        {
-            root.right = Delete(root.right, val);
-        }
-        else
-        {
-            if(val<root.val)
-            {
-                root.left = Delete(root.left, val);
-            }
-            else
-            {
-                if(root.left==null && root.right==null)
-                    root = null;
-                else
-                {
-                    if(root.right!=null)
-                    {
-                        root.val = successor(root,val);
-                        root.right = Delete(root.right, root.val);
-                    }
-                    else
-                    {
-                        root.val = predecessor(root,val);
-                        root.left = Delete(root.left, root.val);
-                    }
-                }
-            }
-            
-        }
-        return root;
-    }
-
-    private int successor(Node root, int val)
-    {
-        root = root.right;
-        while(root.left!=null)
-            root = root.left;
-        
-        return root.val;
-    }
-
-    private int predecessor(Node root,int val)
-    {
-        root = root.left;
-        while(root.right!=null)
-            root= root.right;
-
-        return root.val;
-    }
-}
-
-
 public class MyHashSet {
-    private readonly int HASH = 2003; //prime number
-    BST[] trees;
+
+    private bool[][] nums;
+
     public MyHashSet() {
-        trees = new BST[HASH];
-        for(int i=0;i<trees.Length;i++)
-            trees[i] = new BST();
-    }
-    
-    public int hash(int key)
-    {
-        return key%HASH;
+        nums = new bool[1000][];
     }
 
-    public void Add(int key) 
-    {
-        int hashValue = hash(key);
-        Node root = trees[hashValue].node;
-        trees[hashValue].node = trees[hashValue].Add(root, key); // Fix: assign back
+    private int hash1(int key) {
+        return key % 1000;
     }
 
-    public void Remove(int key) 
-    {
-        int hashValue = hash(key);
-        Node root = trees[hashValue].node;
-        trees[hashValue].node = trees[hashValue].Delete(root, key); // Fix: assign back
+    private int hash2(int key) {
+        return key / 1000;
     }
 
-    public bool Contains(int key) 
-    {
-        int hashValue = hash(key);
-        Node root = trees[hashValue].node;
-        root = trees[hashValue].Search(root, key);
-        return root != null;
+    public void Add(int key) {
+        int i = hash1(key);
+        int j = hash2(key);
+
+        if (nums[i] == null) {
+            nums[i] = (i == 0) ? new bool[1001] : new bool[1000];
+        }
+
+        nums[i][j] = true;
+    }
+
+    public void Remove(int key) {
+        int i = hash1(key);
+        int j = hash2(key);
+
+        if (nums[i] != null) {
+            nums[i][j] = false;
+        }
+    }
+
+    public bool Contains(int key) {
+        int i = hash1(key);
+        int j = hash2(key);
+
+        return nums[i] != null && nums[i][j];
     }
 }
